9c062ae6082de6487d9b22ba9f7c954162eb6448
Shyam prakash <shyamprakash9959@gmail.com>
2023-10-10 21:05:07 +0530

    LVL9

--- smarter-tasks/src/pages/project_details/index.tsx
+++ smarter-tasks/src/pages/project_details/index.tsx
@@ -1,8 +1,7 @@
-import React from "react";
-
-import ProjectDetails from "./ProjectDetails";
+import React, { Suspense } from "react";
+const ProjectDetails = React.lazy(() => import("./ProjectDetails"));
 import { TasksProvider } from "../../context/task/context";
-
+import ErrorBoundary from "../../components/ErrorBoundary";
 import { Outlet } from "react-router-dom";
 import { CommentsProvider } from "../../context/comment/context";
 
@@ -10,7 +9,13 @@ const ProjectDetailsIndex: React.FC = () => {
   return (
     <TasksProvider>
       <CommentsProvider>
-        <ProjectDetails />
+        <ErrorBoundary>
+          <Suspense
+            fallback={<div className="suspense-loading">Loading...</div>}
+          >
+            <ProjectDetails />
+          </Suspense>
+        </ErrorBoundary>
         <Outlet />
       </CommentsProvider>
     </TasksProvider>
